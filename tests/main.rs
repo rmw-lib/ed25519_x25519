243ac40ef482e857d01367cf21b64a7eeb2924ee
gcxfd <i@rmw.link>
2021-06-21 17:21:22 +0800

    🔶

--- tests/main.rs
+++ tests/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
-use ed25519_dalek_blake3::{Keypair, PublicKey, SecretKey};
+use blake3::hash;
+use ed25519_dalek_blake3::{Keypair, PublicKey as Pk, SecretKey as Sk};
 use rand::rngs::OsRng;
+use x25519_dalek::{PublicKey, StaticSecret};
 
 pub fn seed() -> [u8; 32] {
   let mut rng = OsRng {};
@@ -8,37 +10,44 @@ pub fn seed() -> [u8; 32] {
   keypair.secret.to_bytes()
 }
 
+pub fn ed25519_x25519_pk(pk: &Pk) -> PublicKey {
+  use curve25519_dalek::edwards::CompressedEdwardsY;
+  let cey = CompressedEdwardsY::from_slice(pk.as_bytes());
+  let ep = cey.decompress().unwrap();
+  PublicKey::from(*ep.to_montgomery().as_bytes())
+}
+
 #[test]
 fn main() -> Result<()> {
-  /*
-  let ed25519_a = {
-    let sk = SecretKey::from_bytes(&seed())?;
-    let mut skv = sk.as_bytes().to_vec();
-    skv.extend_from_slice(PublicKey::from(&sk).as_bytes());
-    Keypair::from_bytes(&skv as &[u8])?
-  };
-  dbg!(ed25519);
-  */
+  let a_seed = seed();
+  let a_e_sk = Sk::from_bytes(&a_seed)?;
+  let a_e_pk = Pk::from(&a_e_sk);
+  let a_x_pk = ed25519_x25519_pk(&a_e_pk);
+  let a_x_sk = StaticSecret::from(*hash(&a_seed).as_bytes());
 
-  let a_sk = SecretKey::from_bytes(&seed())?;
-  let a_pk = PublicKey::from(&a_sk);
-  println!(
-    r#"a
-sk {:?}
-pk {:?}"#,
-    a_sk.as_bytes(),
-    a_pk.as_bytes()
-  );
+  let b_e_sk = Sk::from_bytes(&seed())?;
+  let b_e_pk = Pk::from(&b_e_sk);
+  let b_x_pk = ed25519_x25519_pk(&b_e_pk);
+  let b_x_sk = StaticSecret::from(*hash(b_e_sk.as_bytes()).as_bytes());
 
-  let b_sk = SecretKey::from_bytes(&seed())?;
-  let b_pk = PublicKey::from(&b_sk);
   println!(
-    r#"b
-sk {:?}
-pk {:?}"#,
-    b_sk.as_bytes(),
-    b_pk.as_bytes()
+    r#"
+{:?}
+
+{:?}"#,
+    b_x_sk.diffie_hellman(&a_x_pk).as_bytes(),
+    a_x_sk.diffie_hellman(&b_x_pk).as_bytes(),
   );
 
   Ok(())
 }
+
+/*
+let ed25519 = {
+  let sk = Sk::from_bytes(&seed())?;
+  let mut skv = sk.as_bytes().to_vec();
+  skv.extend_from_slice(Pk::from(&sk).as_bytes());
+  Keypair::from_bytes(&skv as &[u8])?
+};
+dbg!(ed25519);
+*/
